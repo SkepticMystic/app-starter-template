a5d60fbffbe112ae3eba276f15e072924c5b7c20
SkepticMystic <rossk29@gmail.com>
2025-08-26 11:21:34 +0200

    Just stick to a BASE_URL env var, simple, agnostic

--- src/lib/const/app.ts
+++ src/lib/const/app.ts
@@ -1,23 +1,10 @@
-import { dev } from "$app/environment";
-
-console.log({
-  "VITE:VITE_VERCEL": import.meta.env.VITE_VERCEL,
-  "VITE:VITE_VERCEL_PROJECT_PRODUCTION_URL": import.meta.env
-    .VITE_VERCEL_PROJECT_PRODUCTION_URL,
-});
+import { PUBLIC_BASE_URL } from "$env/static/public";
 
 export const APP = {
   NAME: "App Starter",
-
-  URL: dev
-    ? "http://localhost:5173"
-    : import.meta.env.VITE_VERCEL
-      ? `https://${import.meta.env.VITE_VERCEL_PROJECT_PRODUCTION_URL}`
-      : "https://TODO.com",
-
-  DESCRIPTION: "An awesome app built with SvelteKit and BetterAuth",
-
+  URL: PUBLIC_BASE_URL,
   LOGO_URL: "https://placehold.co/600x400/png",
+  DESCRIPTION: "An awesome app built with SvelteKit and BetterAuth",
 };
 
 console.log({ APP });
