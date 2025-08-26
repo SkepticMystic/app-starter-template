bd41e4d753a7689de0976b8f45bbcac66b28f3c7
SkepticMystic <rossk29@gmail.com>
2025-08-26 12:03:51 +0200

    fix: Set auth paths on client too

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -6,8 +6,12 @@ import {
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/svelte";
 import { AccessControl } from "./auth/permissions";
+import { APP } from "./const/app";
 
 export const BetterAuthClient = createAuthClient({
+  baseURL: APP.URL,
+  basePath: "/api/auth",
+
   plugins: [
     passkeyClient(),
     organizationClient(),
