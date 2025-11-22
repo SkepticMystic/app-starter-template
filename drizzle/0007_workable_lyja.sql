b721854a50cb6a05280b9bbad1779de60946dc5c
SkepticMystic <rossk29@gmail.com>
2025-11-22 08:43:08 +0200

    Update migration

--- drizzle/0007_workable_lyja.sql
+++ drizzle/0007_workable_lyja.sql
@@ -1,30 +1,30 @@
-ALTER TABLE "account" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "account" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "account" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "account" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "invitation" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "account" ALTER COLUMN "user_id" SET DATA TYPE uuid USING user_id::uuid;--> statement-breakpoint
+ALTER TABLE "invitation" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "invitation" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "invitation" ALTER COLUMN "inviter_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "invitation" ALTER COLUMN "organization_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "member" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "invitation" ALTER COLUMN "inviter_id" SET DATA TYPE uuid USING inviter_id::uuid;--> statement-breakpoint
+ALTER TABLE "invitation" ALTER COLUMN "organization_id" SET DATA TYPE uuid USING organization_id::uuid;--> statement-breakpoint
+ALTER TABLE "member" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "member" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "member" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "member" ALTER COLUMN "organization_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "organization" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "member" ALTER COLUMN "user_id" SET DATA TYPE uuid USING user_id::uuid;--> statement-breakpoint
+ALTER TABLE "member" ALTER COLUMN "organization_id" SET DATA TYPE uuid USING organization_id::uuid;--> statement-breakpoint
+ALTER TABLE "organization" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "organization" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "passkey" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "passkey" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "passkey" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "passkey" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "passkey" ALTER COLUMN "user_id" SET DATA TYPE uuid USING user_id::uuid;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "session" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "impersonated_by" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "member_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "session" ALTER COLUMN "active_organization_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "user" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "user_id" SET DATA TYPE uuid USING user_id::uuid;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "impersonated_by" SET DATA TYPE uuid USING impersonated_by::uuid;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "member_id" SET DATA TYPE uuid USING member_id::uuid;--> statement-breakpoint
+ALTER TABLE "session" ALTER COLUMN "active_organization_id" SET DATA TYPE uuid USING active_organization_id::uuid;--> statement-breakpoint
+ALTER TABLE "user" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "user" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "verification" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "verification" ALTER COLUMN "id" SET DATA TYPE uuid USING id::uuid;--> statement-breakpoint
 ALTER TABLE "verification" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();--> statement-breakpoint
-ALTER TABLE "task" ALTER COLUMN "org_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "task" ALTER COLUMN "member_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "task" ALTER COLUMN "user_id" SET DATA TYPE uuid;--> statement-breakpoint
-ALTER TABLE "task" ALTER COLUMN "assigned_member_id" SET DATA TYPE uuid;
+ALTER TABLE "task" ALTER COLUMN "org_id" SET DATA TYPE uuid USING org_id::uuid;--> statement-breakpoint
+ALTER TABLE "task" ALTER COLUMN "member_id" SET DATA TYPE uuid USING member_id::uuid;--> statement-breakpoint
+ALTER TABLE "task" ALTER COLUMN "user_id" SET DATA TYPE uuid USING user_id::uuid;--> statement-breakpoint
+ALTER TABLE "task" ALTER COLUMN "assigned_member_id" SET DATA TYPE uuid USING assigned_member_id::uuid;
