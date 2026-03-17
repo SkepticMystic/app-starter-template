db413b9a45fe4bb3f5b6303b3eb769746ec60a00
SkepticMystic <rossk29@gmail.com>
2026-03-17 11:56:55 +0200

    import

--- src/lib/utils/html/html.util.ts
+++ src/lib/utils/html/html.util.ts
@@ -1,6 +1,6 @@
 import type { Branded } from "$lib/interfaces/zod/zod.type";
-import { sanitize } from "isomorphic-dompurify";
+import Purify from "isomorphic-dompurify";
 
 export const HTMLUtil = {
-  sanitize: (dirty: string) => sanitize(dirty) as Branded<"SanitizedHTML">,
+  sanitize: (dirty: string) => Purify.sanitize(dirty) as Branded<"SanitizedHTML">,
 };
